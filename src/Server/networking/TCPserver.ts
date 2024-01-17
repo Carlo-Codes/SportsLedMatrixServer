47a295218c12fc43e6d8dda097cff825aebc0eed
Carlo <carlo.scrocca@googlemail.com>
2024-01-17 18:12:02 +0000

    sending data via tcp

---
+++ src/Server/networking/TCPserver.ts
@@ -0,0 +1,55 @@
+import net, { Socket } from 'net'
+
+export let sockets:Socket[] = []
+
+
+export const TCPserver = net.createServer((socket:Socket) => {
+    const deviceinfo = { //porbably wont need this
+        name:"RasberryPi",
+        ip:socket.remoteAddress
+    }
+    console.log('clientConnected')
+    sockets.push(socket);
+
+    socket.on('end', () => {
+        console.log('client Disconnected')
+        let index = sockets.indexOf(socket)
+        if(index !== -1){
+          sockets.splice(index, 1)
+        }
+    })
+    socket.write('hello from the server! \r\n')
+})
+
+TCPserver.on('error', (err:unknown) => {
+    if(err instanceof Error){
+        throw err
+    }
+})
+
+
+// telnet localhost 3001
+
+
+
+
+/* const net = require('net');
+
+const serverPort = 12345; // Replace with the actual port number
+
+const server = net.createServer((socket) => {
+  // Send information about the device when a connection is established
+  const deviceInfo = {
+    name: 'MyDevice',
+    ip: socket.remoteAddress,
+    // Add other information as needed
+  };
+
+  socket.write(JSON.stringify(deviceInfo));
+  socket.end();
+});
+
+server.listen(serverPort, () => {
+  console.log(`TCP server listening on port ${serverPort}`);
+});
+ */
--- src/Server/server.ts
+++ src/Server/server.ts
@@ -1,11 +1,17 @@
 import express from 'express'
 import * as path from 'path';
-
+import { TCPserver, sockets } from './networking/TCPserver';
 const port = 3000
+const TCPport = 3001
 
 const app = express()
 
 app.use(express.static('../Client'))
+
+TCPserver.listen(TCPport, () => {
+  console.log('got Ya')  
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
@@ -13,5 +19,11 @@ app.listen(port, () => {
 
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../Client/test.html'))
-    
+    for(let i = 0; i < sockets.length; i++){
+      sockets[i].write('<0x01>Z00<0x02>AA<0x06><0x0a>I0ABCD2345<0x0D>EFGH6789<0x0D>IJKL0123<0x04>')
+  }
   })
+
+
+
+
