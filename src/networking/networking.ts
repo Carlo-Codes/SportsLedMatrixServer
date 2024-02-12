4dfcdb1d25e728afe82a0d46c96dd77b7ab002d2
Carlo <carlo.scrocca@googlemail.com>
2024-02-12 16:53:31 +0000

    restructured

--- src/networking/networking.ts
+++ src/networking/networking.ts
@@ -1,17 +1,31 @@
 import { spawn, ChildProcessWithoutNullStreams } from "child_process"
 
+interface arpScanRes{
+  ip:string,
+  mac:string
+}
+
 export class Networking{
     ipAddress:string|undefined = ''
     networkInterfaces:object = {}
     arpScan:ChildProcessWithoutNullStreams
     arpScanCommand = 'arp-scan';
     arpScanArgs = ['192.168.1.0/24'];
+    scannedIps:arpScanRes[] = []
+
 
     constructor(){
         this.arpScan = spawn('sudo', [this.arpScanCommand, ...this.arpScanArgs])
 
-        this.arpScan.stdout.on('data', (data) => {
+        this.arpScan.stdout.on('data', (data:string) => {
             console.log(`stdout: ${data}`);
+            const parsedData = data.split('\t')
+            const scannedIp:arpScanRes = {
+              ip:parsedData[0],
+              mac:parsedData[1]
+            }
+            this.scannedIps.push(scannedIp)
+
           });
           
           this.arpScan.stderr.on('data', (data) => {
